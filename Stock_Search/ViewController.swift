1f4b4346a03f4febc9e38528d9f9c475fe511c3d
Hey!Alfred <yu.gao.whut@gmail.com>
2016-04-20 11:42:51 -0700

    Initial Commit

---
+++ Stock_Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stock_Search
+//
+//  Created by 高宇 on 4/20/16.
+//  Copyright © 2016 Alfred_Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
