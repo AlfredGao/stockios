d94cb734177083355b94511a7543534f9618bb67
Hey!Alfred <yu.gao.whut@gmail.com>
2016-04-20 20:22:32 -0700

    impiementing autocomplete

--- Stock_Search/ViewController.swift
+++ Stock_Search/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CCAutocomplete
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AutocompleteDelegate{
 
+    @IBOutlet weak var stock_search_label: UILabel!
+    @IBOutlet weak var stock_search_field: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    //MARK: Text
+    
+    
+    //MARK: Conform the CCAutocomplete Protocol
+    func autoCompleteTextField() -> UITextField {
+        return stock_search_field
+    }
+    
+    func autoCompleteThreshold(textField: UITextField) -> Int {
+        return 1
+    }
+    
+    func autoCompleteItemsForSearchTerm(term: String) -> [AutocompletableOption] {
+        let cell =  AutocompleteCellData(text:  "AAAA", image: nil)
+        let cell2 = AutocompleteCellData(text: "BBBB", image: nil)
+        let cell3 = AutocompleteCellData(text: "CCCC", image: nil)
+        let cell4 = AutocompleteCellData(text: "DDDD", image: nil)
+        
+        let test = [cell,cell2, cell3, cell4]
+        
+        return test
+    }
+    
+    func autoCompleteHeight() -> CGFloat {
+        return 500
+    }
+    
+    func didSelectItem(item: AutocompletableOption) {
+        stock_search_label.text = item.text
+    }
+    }
 
